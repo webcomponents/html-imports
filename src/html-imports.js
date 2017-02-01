30a4b6feff77fb73708110fe4eacf90f0c23a0b2
valdrinkoshi <valdrin@google.com>
2017-02-01 13:06:45 -0800

    accept type text/css

--- src/html-imports.js
+++ src/html-imports.js
@@ -227,10 +227,11 @@
 
   const disabledLinkSelector = `link[rel=stylesheet][href][type=${importDisableType}]`;
 
-  const importDependenciesSelector = `${importSelector}, ${disabledLinkSelector},
-    style:not([type]), link[rel=stylesheet][href]:not([type]),
-    script:not([type]), script[type="application/javascript"],
-    script[type="text/javascript"]`;
+  const importDependenciesSelector = `${importSelector},
+style:not([type]), style[type="text/css"], ${disabledLinkSelector},
+link[rel=stylesheet][href]:not([type]), link[rel=stylesheet][href][type="text/css"],
+script:not([type]), script[type="application/javascript"],
+script[type="text/javascript"]`;
 
   const importDependencyAttr = 'import-dependency';
 
@@ -239,7 +240,7 @@
   const pendingScriptsSelector = `script[${importDependencyAttr}]`;
 
   const pendingStylesSelector = `style[${importDependencyAttr}],
-    link[rel=stylesheet][${importDependencyAttr}]`;
+link[rel=stylesheet][${importDependencyAttr}]`;
 
   /**
    * @type {Function}
